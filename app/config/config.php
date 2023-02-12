cbe3f73e28c0813e3d51822f7189336054ef0f24
Kreutxer <danihorison65@gmail.com>
2023-02-12 20:24:42 +0700

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define('BASEURL', 'http://localhost:8080/bukubekas/public');
-
+// define('BASEURL', 'http://localhost:8080/bukubekas/public');
+define('BASEURL', 'http://localhost/bukubekas/public');
 // DB
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
